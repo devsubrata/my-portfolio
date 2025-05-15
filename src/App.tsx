eb54bc576d89fbfae00dd32bb341d6f365cc8c80
devsubrata <devsubrata@gmail.com>
2025-05-16 00:00:14 +0600

    Github URL corrected

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,14 @@ import "./App.css";
 function App() {
     return (
         <div className="App">
+            <div className="profile_pic">
+                <img src="https://picsum.photos/300/300" />
+            </div>
             <h1>Hi, I'm Jane Doe</h1>
             <p>I'm a Frontend Developer skilled in React and TypeScript.</p>
             <ul>
                 <li>
-                    <a href="https://github.com/yourusername" target="_blank">
+                    <a href="https://github.com/devsubrata" target="_blank">
                         GitHub
                     </a>
                 </li>
