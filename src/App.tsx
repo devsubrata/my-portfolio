85c7be52312c3d48736b98971793e2fa4f7272cd
devsubrata <devsubrata@gmail.com>
2025-05-15 23:40:07 +0600

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import "./App.css";
+
+function App() {
+    return (
+        <div className="App">
+            <h1>Hi, I'm Jane Doe</h1>
+            <p>I'm a Frontend Developer skilled in React and TypeScript.</p>
+            <ul>
+                <li>
+                    <a href="https://github.com/yourusername" target="_blank">
+                        GitHub
+                    </a>
+                </li>
+                <li>
+                    <a href="mailto:you@example.com">Email Me</a>
+                </li>
+            </ul>
+        </div>
+    );
+}
+
+export default App;
